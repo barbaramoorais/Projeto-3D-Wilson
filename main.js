70e053fde738e37908c8c7d516a4b407023d1d4a
barbara <106076374+barbaramoorais@users.noreply.github.com>
2024-03-12 10:19:02 -0300

    aaaaaaa

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 const botoes = document.querySelectorAll(".botao")
 
-for (let i = 0)
+for (let i = 0; i < botoes.length; j++){
+    botoes[i].onclick= function (){
+        botoes [i].classList.add("ativo")
+        for (let j = 0; j < botoes.length; j++){
+            botoes [j].classList.remove(ativo);
+        }
+        botoes[i].classList.add("ativo")
+    }
+}
