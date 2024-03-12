bcf747d78dac533d98c86872a61a26d459e377ee
barbara <106076374+barbaramoorais@users.noreply.github.com>
2024-03-12 10:25:09 -0300

    aaaaa

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick= function (){
         botoes [i].classList.add("ativo")
         for (let j = 0; j < botoes.length; j++){
-            botoes [i].classList.remove("ativo");
+            botoes [j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo")
     }
