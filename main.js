150abc6f0d7db4184ef59c07da93b5ec6fd8f96f
barbara <106076374+barbaramoorais@users.noreply.github.com>
2024-03-12 10:19:49 -0300

    s

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const botoes = document.querySelectorAll(".botao")
 
-for (let i = 0; i < botoes.length; j++){
+for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick= function (){
         botoes [i].classList.add("ativo")
         for (let j = 0; j < botoes.length; j++){
