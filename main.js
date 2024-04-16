599a2d472b1c719474cff45daf3906eb0843a68a
barbara <106076374+barbaramoorais@users.noreply.github.com>
2024-04-16 10:54:23 -0300

    aaaaaaaa

--- main.js
+++ main.js
@@ -14,7 +14,12 @@ for (let i = 0; i < botoes.length; i++){
 }
 const contadores = document.querySelectorAll(".contador");
 const tempoobjetivo1 = new Date ("2024-12-01T00:00:00");
-let tempoAtual = new Date ();
+function calculaTempo(tempoobjetivo1){
+    let tempoAtual = new Date ();
+    contadores[0].textContent =tempoobjetivo1 - tempoAtual;
+    let segundos = Math.floor(tempofinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor (minutos / 60);
+    let dis = Math.floor (horas / 24);
+}    
 
-
-contadores[0].textContent =tempoobjetivo1 - tempoAtual;
