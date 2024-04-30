7ac76c5a4f8f1216fd0d3953dcaed58448855129
barbara <106076374+barbaramoorais@users.noreply.github.com>
2024-04-30 11:11:37 -0300

    aaaa

--- main.js
+++ main.js
@@ -16,6 +16,8 @@ const tempoObjetivo1 = new Date ("2024-12-01T23:59:59");
 const tempoObjetivo2 = new Date ("2024-12-01T23:59:59");
 const tempoObjetivo3 = new Date ("2024-12-01T23:59:59");
 const tempoObjetivo4 = new Date ("2024-12-01T23:59:59");
+const tempoObjetivo5 = new Date ("2024-12-12T23:59:59");
+
 
 const tempos = [
     tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
