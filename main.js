cd90119b57444ce5091eef807f28e5ddf5359294
barbara <106076374+barbaramoorais@users.noreply.github.com>
2024-04-30 10:24:57 -0300

    aaaa

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll (".aba-conteudo");
 
-for (let i = 0; i < botoes.length; i++){
+for (let i = 0; i<botoes.length; i++){
     botoes[i].onclick= function (){
         botoes [i].classList.add("ativo")
         for (let j = 0; j < botoes.length; j++){
-            botoes [j].classList.remove("ativo");
-            textos [j].classList.remove("ativo");
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
