11335475449ef1c1cea98a109e8cbdf8b536948b
barbara <106076374+barbaramoorais@users.noreply.github.com>
2024-05-17 09:07:43 -0300

    a

--- main.js
+++ main.js
@@ -12,10 +12,10 @@ for (let i = 0; i<botoes.length; i++) {
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-28T23:59:59");
-const tempoObjetivo2 = new Date("2024-12-28T23:59:59");
-const tempoObjetivo3 = new Date("2024-12-28T23:59:59");
-const tempoObjetivo4 = new Date("2024-12-28T23:59:59");
+const tempoObjetivo1 = new Date("2024-12-12T08:59:59");
+const tempoObjetivo2 = new Date("2024-12-15T21:59:59");
+const tempoObjetivo3 = new Date("2024-12-13T23:59:59");
+const tempoObjetivo4 = new Date("2024-12-19T21:16:16");
 
 const tempos = [
     tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
